cba3830a87c156af24dea61d63b62b365b12acab
nsaripalli <contact@nithin.info>
2021-10-25 09:10:15 -0400

    backend MVP done

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a very similar application to http://www.yourtrainingcalendar.com/
+I wanted more training plan options, so I went ahead and threw something together
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+from fastapi import FastAPI, HTTPException
+import os
+import pandas as pd
+import datetime as dt
+from fastapi.responses import Response
+from icalendar import Calendar, Event
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.get("/list/schedules")
+def list_options():
+    """
+    Lists the available training plan options
+    """
+    return os.listdir("schedules")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/items/generate_ical")
+def generate_ical(schedule_name: str, start_date: dt.date):
+    """
+    Generates an ical for a training schedule with a given starting date
+    """
+    if not schedule_name in os.listdir("schedules"):
+        return HTTPException(status_code=422, detail="Please choose an option amongst the available schedule options")
+    full_schedule_path = f"schedules/{schedule_name}"
+    # Create a table of events based on schedule
+    schedule = pd.read_csv(full_schedule_path, header=None, skiprows=1, names=["description"]).reset_index()
+    with open(full_schedule_path) as f:
+        schedule.about = f.readline()
+    schedule['date'] = schedule['index'].apply(lambda x: start_date + dt.timedelta(days=x))
+    # Load events into Calendar
+    cal = Calendar()
+    cal.add('prodid', '-//Fast Walking//Running Training Planner//EN')
+    cal.add('version', '2.0')
+    for index, row in schedule.iterrows():
+        cal.add_component(Event({"summary": row.description, "DTSTART;VALUE=DATE": row.date.strftime("%Y%m%d"),
+                                 "DTEND;VALUE=DATE": (row.date + dt.timedelta(days=1)).strftime("%Y%m%d"),
+                                 "description": f"For details, click here {schedule.about}"}))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return Response(cal.to_ical(), media_type="text/calendar")
